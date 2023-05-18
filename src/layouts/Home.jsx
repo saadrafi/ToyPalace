90a6882afb8bc7fbe34bd34753e0dd7169fc71de
saadrafi <saadrafi81@gmail.com>
2023-05-18 12:59:55 +0600

    footer design finished

--- src/layouts/Home.jsx
+++ src/layouts/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../components/navbar/Navbar";
+import Footer from "../components/footer/Footer";
 
 const Home = () => {
   return (
     <div>
       <Navbar></Navbar>
+      <Footer></Footer>
     </div>
   );
 };
