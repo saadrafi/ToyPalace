b6cdf5e7cec00e12c8b4059a25200a8ce9ceee0a
saadrafi <saadrafi81@gmail.com>
2023-05-19 23:20:07 +0600

    change design

--- src/pages/home/photos.js
+++ src/pages/home/photos.js
@@ -19,13 +19,20 @@ export const photos = [
     width: 3,
     height: 4,
   },
+
+  {
+    src: "https://www.lego.com/cdn/cs/set/assets/blt04e5e6562a4e59e0/76255_alt1.png",
+    width: 4,
+    height: 3,
+  },
+
   {
     src: "https://www.lego.com/cdn/cs/set/assets/blta72453da5ddeb718/76214_alt2.png",
-    width: 3,
+    width: 5,
     height: 4,
   },
   {
-    src: "https://www.lego.com/cdn/cs/set/assets/blt04e5e6562a4e59e0/76255_alt1.png",
+    src: "https://www.lego.com/cdn/cs/set/assets/bltb89694196303ee59/75571_alt1.png",
     width: 4,
     height: 3,
   },
@@ -33,10 +40,5 @@ export const photos = [
     src: "https://www.lego.com/cdn/cs/set/assets/blt56b4ec4e20d3d82d/76413_alt1.png",
     width: 4,
     height: 4,
-  },
-  {
-    src: "https://www.lego.com/cdn/cs/set/assets/bltb89694196303ee59/75571_alt1.png",
-    width: 4,
-    height: 3,
   },
 ];
