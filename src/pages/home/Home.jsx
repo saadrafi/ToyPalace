0877ff6854f96c42eaf4b58b48b06aac3f29af7f
saadrafi <saadrafi81@gmail.com>
2023-05-20 23:58:18 +0600

    AOS implemented in home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,15 +2,25 @@ import React from "react";
 import Banner from "./Banner";
 import Galllery from "./Galllery";
 import CategoryTabs from "./categoryTabs/CategoryTabs";
+import AOS from "aos";
+import "aos/dist/aos.css";
+// ..
+AOS.init();
 
 const Home = () => {
-  return <div>
-    <Banner></Banner>
-
-    <Galllery></Galllery>
-
-    <CategoryTabs></CategoryTabs>
-  </div>;
+  return (
+    <div>
+      <div data-aos="fade-in">
+        <Banner></Banner>
+      </div>
+      <div data-aos="zoom-in" data-aos-duration="3000">
+        <Galllery></Galllery>
+      </div>
+      <div data-aos="fade-right">
+        <CategoryTabs></CategoryTabs>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
